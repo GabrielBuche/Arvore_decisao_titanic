a43ac783f02dc513ecc2c4c59b5a1a1d9c078510
gabriel buche <bielbuche@gmail.com>
2023-05-24 20:00:03 -0300

    correçao do tratamento dos dados

--- titanic.py
+++ titanic.py
@@ -1,30 +1,67 @@
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
 import pickle
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('titanic_tratado.csv')
-data['Sex'] = data['Sex'].replace({'male': 0, 'female': 1})
-data['Embarked'] = data['Embarked'].replace({'S': 10, 'Q':20, 'C': 30})
+data = pd.read_csv('titanic.csv')
 
-X = data.drop('Survived', axis=1)
-Y = data['Survived']
+def tratar_dados(data):
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    # Troca sexo masculino para 0 e feminino para 1
+    # Troca a  cidade Cherbourg (C) para 0
+    # Troca a  cidade Queenstown (Q) para 1
+    # Troca a  cidade Southampton (S) para 2
 
-arvoreDecisao = DecisionTreeClassifier(criterion='entropy',max_depth=6)
+    data['Sex'] = data['Sex'].replace({'male': 0, 'female': 1})
+    data['Embarked'] = data['Embarked'].replace({'C': 0, 'Q': 1, 'S': 2})
+    data = data.drop(['PassengerId','Name','Ticket','Cabin'])
 
-arvoreDecisao.fit(X_train, y_train)
+    data_completa = data[data['Age'].notna()]
+    data_completa =  data_completa.dropna()
+    data_incompleta = data[data['Age'].isna()]
 
-accuracy = arvoreDecisao.score(X_test, y_test)
+    X_complete = data_completa.drop('Age', axis=1)
 
-print(accuracy * 100)
+    X_incomplete = data_incompleta.drop('Age', axis=1)
+    y_complete = data_completa['Age']
 
-with open("arvore_decisao.pkl", "wb") as f:
-    pickle.dump(arvoreDecisao, f)
+    arvore_Age = DecisionTreeRegressor(criterion='entropy',max_depth=6)
 
-tree.plot_tree(arvoreDecisao)
-plt.show()
+    arvore_Age.fit(X_complete, y_complete)
+
+    age_predictions = arvore_Age.predict(X_incomplete)
+
+    data_incompleta['Age'] = age_predictions
+
+    data_filled = pd.concat([data_completa, data_incompleta])
+
+    return data_filled
+
+def gerar_arvore_decisao():
+    data = pd.read_csv('titanic_tratado.csv')
+
+    X = data.drop('Survived', axis=1)
+    Y = data['Survived']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    arvoreDecisao = DecisionTreeClassifier(criterion='entropy',max_depth=6)
+
+    arvoreDecisao.fit(X_train, y_train)
+
+    accuracy = arvoreDecisao.score(X_test, y_test)
+
+    print(accuracy * 100)
+
+    with open("arvore_decisao.pkl", "wb") as f:
+        pickle.dump(arvoreDecisao, f)
+
+    tree.plot_tree(arvoreDecisao)
+    plt.show()
+
+def gerar_perceptron():
+    print('perceptron')
